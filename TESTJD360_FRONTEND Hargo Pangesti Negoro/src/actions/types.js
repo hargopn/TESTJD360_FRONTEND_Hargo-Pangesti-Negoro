b4c04b56430cf667cfa2958cb123075295b7cf29
hargopn <67982138+hargopn@users.noreply.github.com>
2020-08-04 14:54:51 +0700

    Add files via upload

---
+++ TESTJD360_FRONTEND Hargo Pangesti Negoro/src/actions/types.js
@@ -0,0 +1,11 @@
+// users
+export const FETCH_USERS = 'FETCH_USERS';
+export const CREATE_USER = 'CREATE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const FETCH_ONE_USER = 'FETCH_ONE_USER';
+export const CANCEL = 'CANCEL';
+export const CHANGE_EVENT = 'CHANGE_EVENT';
+
+// pagination
+export const SELECT_PAGE = 'SELECT_PAGE';
